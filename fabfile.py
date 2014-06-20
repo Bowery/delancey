a17995ebdac4d950774137e7b0e6ae3585152fb4
Steve Kaliski <sjkaliski@gmail.com>
2014-06-20 12:39:28 -0400

    deploy and set up satellite

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,8 @@ hosts = [
   'bowery@64.71.188.179',
   'ubuntu@ec2-54-205-254-102.compute-1.amazonaws.com'
 ]
-env.key_filename = '/home/ubuntu/.ssh/id_aws'
-# env.key_filename = '/Users/steve/.ssh/bowery.pem'
+# env.key_filename = '/home/ubuntu/.ssh/id_aws'
+env.key_filename = '/Users/steve/.ssh/bowery.pem'
 env.password = 'java$cript'
 
 @parallel
@@ -24,11 +24,11 @@ def restart():
     with cd('delancey'):
       sudo('GOPATH=' + go_path + ' go get -d')
       sudo('GOPATH=' + go_path + ' go build')
-      # sudo('mv delancey satellite')
-      # sudo('cp -f satellite ' + project_path + '/images/bowery/')
-      # sudo('rm -rf /satellite/*')
-      # sudo('cp -rf ' + project_path + '/images/bowery/* /satellite/')
-      # sudo('chown ' + local_username + ':' +local_username + ' /satellite/satellite')
+      sudo('mv delancey satellite')
+      sudo('cp -f satellite ' + project_path + '/images/bowery/')
+      sudo('rm -rf /satellite/*')
+      sudo('cp -rf ' + project_path + '/images/bowery/* /satellite/')
+      sudo('chown ' + local_username + ':' +local_username + ' /satellite/satellite')
 
 
 def deploy():
