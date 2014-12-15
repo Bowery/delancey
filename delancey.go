13041e6486fa109e8a3cc6d3448de008f2b76176
Mitch Pirtle <mitch@spacemonkeylabs.com>
2014-12-15 12:59:35 -0500

    Moving debugging text to helpful message.

--- delancey.go
+++ delancey.go
@@ -36,7 +36,7 @@ func main() {
 		fmt.Println(VERSION)
 		os.Exit(0)
 	}
-	fmt.Println(dockerAddr)
+	fmt.Println("Starting up Delancey with Docker at ", dockerAddr)
 	DockerClient, err = docker.NewClient(dockerAddr)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
